e8f07cd45a130b4645646d9e7323b938e019a1e9
Fellteros <144528463+Fellteros@users.noreply.github.com>
2025-09-08 16:40:21 +0200

    Add @Environment annotations where suitable

--- src/main/java/net/fellter/moreLeafParticles/client/MoreLeafParticlesClient.java
+++ src/main/java/net/fellter/moreLeafParticles/client/MoreLeafParticlesClient.java
@@ -6,14 +6,18 @@ import net.fellter.moreLeafParticles.ModParticles;
 import net.minecraft.client.particle.LeavesParticle;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 
+@Environment(EnvType.CLIENT)
 public class MoreLeafParticlesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ParticleFactoryRegistry.getInstance().register(ModParticles.SPRUCE_NEEDLES, LeavesParticle.TintedLeavesFactory::new);
 		ParticleFactoryRegistry.getInstance().register(ModParticles.BIRCH_LEAVES, LeavesParticle.TintedLeavesFactory::new);
 		ParticleFactoryRegistry.getInstance().register(ModParticles.MANGROVE_LEAVES, LeavesParticle.TintedLeavesFactory::new);
+		ParticleFactoryRegistry.getInstance().register(ModParticles.JUNGLE_LEAVES, LeavesParticle.TintedLeavesFactory::new);
 		ParticleFactoryRegistry.getInstance().register(ModParticles.AZALEA_LEAVES, ModLeavesParticle.SimpleLeavesFactory::new);
 		ParticleFactoryRegistry.getInstance().register(ModParticles.FLOWERING_AZALEA_PARTICLES, ModLeavesParticle.SimpleLeavesFactory::new);
 	}
