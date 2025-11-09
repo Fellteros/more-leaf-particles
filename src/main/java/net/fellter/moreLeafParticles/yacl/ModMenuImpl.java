cff2a664a9ebae812ef357b723fd46d4a864c40e
Fellteros <144528463+Fellteros@users.noreply.github.com>
2025-11-09 10:18:04 +0100

    rehaul YACL check

--- src/main/java/net/fellter/moreLeafParticles/yacl/ModMenuImpl.java
+++ src/main/java/net/fellter/moreLeafParticles/yacl/ModMenuImpl.java
@@ -3,11 +3,12 @@ package net.fellter.moreLeafParticles.yacl;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import com.terraformersmc.modmenu.api.UpdateChecker;
+import net.fellter.moreLeafParticles.MoreLeafParticles;
 
 public class ModMenuImpl implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")
+		return MoreLeafParticles.isYACLPresent()
 				? parentScreen -> YACLImpl.create().generateScreen(parentScreen)
 				: ModMenuApi.super.getModConfigScreenFactory();
 	}
