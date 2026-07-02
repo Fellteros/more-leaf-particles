3ff935c6ef29ade785655c65b8d5f9d5e0efb984
Fellteros <144528463+Fellteros@users.noreply.github.com>
2026-07-02 12:39:42 +0200

    Fix #28 - set correct required Java version based on current version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,14 @@ val accesswidener = when {
 	else -> "1.21.5-1.21.8.ct"
 }
 
+val requiredJava: JavaVersion = when {
+	sc.current.parsed >= "26.1" -> JavaVersion.VERSION_25
+	sc.current.parsed >= "1.20.5" -> JavaVersion.VERSION_21
+	sc.current.parsed >= "1.18" -> JavaVersion.VERSION_17
+	sc.current.parsed >= "1.17" -> JavaVersion.VERSION_16
+	else -> JavaVersion.VERSION_1_8
+}
+
 base {
 	archivesName = sc.property("archives_base_name")
 }
@@ -121,18 +129,16 @@ tasks.processResources {
 	}
 }
 
-val targetJavaVersion = 25
 
 java {
-	val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-	if (JavaVersion.current() < javaVersion) {
-		toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
-	}
-
-	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
-	// if it is present.
-	// If you remove this line, sources will not be generated.
 	withSourcesJar()
+	targetCompatibility = requiredJava
+	sourceCompatibility = requiredJava
+
+	toolchain {
+		vendor = JvmVendorSpec.ORACLE
+		languageVersion = JavaLanguageVersion.of(requiredJava.majorVersion)
+	}
 }
 
 kotlin {
